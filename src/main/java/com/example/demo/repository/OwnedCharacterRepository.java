c37c345036903a3d54049340bcb62743580b92af
Takaki Takeda(武田卓樹) <114552897+ttttt157@users.noreply.github.com>
2026-02-05 09:53:45 +0900

    Update OwnedCharacterRepository.java

--- src/main/java/com/example/demo/repository/OwnedCharacterRepository.java
+++ src/main/java/com/example/demo/repository/OwnedCharacterRepository.java
@@ -3,6 +3,8 @@ package com.example.demo.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.OwnedCharacter;
@@ -14,4 +16,9 @@ public interface OwnedCharacterRepository extends JpaRepository<OwnedCharacter, 
 	boolean existsByUsernameAndOwnedcharacter(String username, String charactername);
 
     // 追加: 所持キャラ登録は save() で可能なので追加メソッド不要
+	   // ★ 追加（レアガチャ用）
+    @Query("SELECT o.id FROM OwnedCharacter o WHERE o.username = :username")
+    List<Integer> findOwnedCharacterIdsByUsername(@Param("username") String username);
+
+
 }
